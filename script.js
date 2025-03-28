00feb90741bdebe3140b10794b07c50ad4176d4e
ozzy <businessozzy27@gmail.com>
2025-03-28 19:31:13 -0400

    a

--- script.js
+++ script.js
@@ -5,11 +5,11 @@ function animatePie() {
     const diff = 40 - percentage;
     if (Math.abs(diff) > .1) {
         percentage += diff * .05;
-        pieChart.style.background = `conic-gradient(#3498db 0% ${percentage}%, #ddd ${percentage}% 100%)`; //monkey patch lol
+        pieChart.style.background = `conic-gradient(#343638 0% ${percentage}%, #959699 ${percentage}% 100%)`; //monkey patch lol
         requestAnimationFrame(animatePie);
     } else {
         percentage = 40;
-        pieChart.style.background = `conic-gradient(#3498db 0% ${percentage}%, #ddd ${percentage}% 100%)`;
+        pieChart.style.background = `conic-gradient(#343638 0% $s{percentage}%, #959699 ${percentage}% 100%)`;
     }
 }
 
